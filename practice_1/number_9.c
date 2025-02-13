d356e4b1a790df8728c822d4af99745f4607ae73
daniil <daniil@pisarev.server.test>
2025-02-13 11:45:48 +0300

    Task #9

---
+++ practice_1/number_9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    double t;
+
+    printf("Введите время передачи сообщения в секундах: ");
+    scanf("%lf", &t);
+
+    if (t >= 0) {
+        int minute = t / 60;
+        printf("Ваше сообщение будет передаваться %d минут %lf секунд \n", minute, (t - (60 * minute)));
+    } else {
+        printf("Ваше сообщение не может быть передано \n");
+    }
+}
